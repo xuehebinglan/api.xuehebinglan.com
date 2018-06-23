ed82e16dd5c6d2c183077bc461a36c6ffe4a0087
xuehebinglan <785529534@qq.com>
2018-06-23 23:14:41 +0800

    增加跨域

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.use('/static', express.static('public'));
 
 var routerFunctions = require('./src/index')
 
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 // let apiPath = {
 //   '/': function (req, res) {
 //     res.sendFile( __dirname + '/' + 'index.html')
